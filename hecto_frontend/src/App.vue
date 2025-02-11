d3ad994d69b5d4523702e8308b349af7f908618d
yasmeenq <yasmeen.qawasmy@gmail.com>
2025-02-12 01:59:04 +0200

    created messages static page

--- hecto_frontend/src/App.vue
+++ hecto_frontend/src/App.vue
@@ -19,7 +19,7 @@
                         </svg>
                     </RouterLink>
 
-                    <RouterLink to="/chat">
+                    <RouterLink to="/messages">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
                         </svg>                              
@@ -39,9 +39,8 @@
                 </div>
 
                 <div class="menu-right">
-                    <a href="#">
-                        <img src="./assets/images/pooh_baeb7dc6.jpeg" class="w-12 h-12 rounded-full  border border-gray-300" />
-                    </a>
+                    <RouterLink to="/signup" class="inline-block py-2 px-4 mr-3 bg-gray-400 text-white text-xs rounded-lg" >Signup</RouterLink>
+                    <RouterLink to="/login" class="inline-block py-2 px-4 mr-3 bg-pink-800 text-white text-xs rounded-lg" >Login</RouterLink>
                 </div>
             </div>
         </div>
--- hecto_frontend/src/router/index.js
+++ hecto_frontend/src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '@/views/HomeView.vue'
 import SignupView from '@/views/SignupView.vue'
 import LoginView from '@/views/LoginView.vue'
 import FeedView from '@/views/FeedView.vue'
+import MessagesView from '@/views/MessagesView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,7 +32,12 @@ const router = createRouter({
         name: 'feed',
         component: FeedView
 
-    }
+    },
+    {
+        path: '/messages',
+        name: 'messages',
+        component: MessagesView
+    },
   ],
 })
 
